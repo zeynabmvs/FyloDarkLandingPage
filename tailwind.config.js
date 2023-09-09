4a5306c150daabc3f92110d47bc4f75384b7ef6b
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2023-09-09 16:44:55 +0330

    section 3 done

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,21 @@ module.exports = {
             // 'tablet-down': { 'max': '768px'},
         // },
         extend: {
+            fontSize: {
+                'zey-header': ['3rem', {
+                    lineHeight: '4rem',
+                    fontWeight: '400',
+                }],
+                'zey-p': ['1.25rem', {
+                    lineHeight: '1.5rem',
+                    fontWeight: '400',
+                }],
+
+            },
             colors: {
                 'primary':{
-                    DEFAULT: '#81BFA8',
-                    light: '#71A894',
+                    DEFAULT: '#71A894',
+                    lighter: '#81BFA8',
                 } ,
                 'gray': {
                     100: '#807E76',
@@ -33,26 +44,14 @@ module.exports = {
                 'white': '#FFFFFF',
                 'beige': '#F9F9F9',
             },
-            boxShadow: {
-                // 'base': '0px 1px 3px 0px rgba(0, 0, 0, 0.10), 0px 1px 2px -1px rgba(0, 0, 0, 0.10)',
-            },
             fontFamily: {
-                // 'primary': "IRANYekanX, serif",
                 'display': "DreamAvenue, serif",
                 'roboto': "Roboto, sans-serif",
-},
+                'code': "Encode Sans, sans-serif",
+            },
 
             backgroundImage: {
-                // 'dark-gradient-meta-cell': 'linear-gradient(270deg, #334155 0%, rgba(51, 65, 85, 0.51) 48.81%, rgba(51, 65, 85, 0.00) 100%)',
-                // 'dark-gradient-show-more-box': 'linear-gradient(360deg, #1E293B 0%, rgba(30, 41, 59, 0.78) 23.96%, rgba(30, 41, 59, 0.00) 100%)',
-                // 'gradient-red' : 'linear-gradient(180deg, #F63326 0%, #F63326 62.24%, rgba(255, 255, 255, 0.00) 100%)',
-                // 'download-box': 'url("../assets/img/doc-download.svg")',
-                // 'download-box-dark': 'url("../assets/img/doc-download-dark.svg")',
             },
-            borderRadius: {
-                // 'base': '0.5rem'
-            }
-
         },
     },
     daisyui: {
